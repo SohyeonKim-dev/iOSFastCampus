4784cb490db07336694b7f4ab2dd5bbe4717e033
SoHyeon Kim <happythgus89@gmail.com>
2022-07-05 14:20:15 +0900

    delegate pattern

--- Part2 Basic/chap2 LED 전광판 앱/LEDBoard/LEDBoard/SettingViewController.swift
+++ Part2 Basic/chap2 LED 전광판 앱/LEDBoard/LEDBoard/SettingViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol LEDBoardSettingDelegate: AnyObject {
+    func changeSetting(text: String?, textColor: UIColor?, backgroundColor: UIColor?)
+}
+
 class SettingViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
@@ -19,32 +23,56 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var orangeButton: UIButton!
     
+    weak var delegate: LEDBoardSettingDelegate?
+    var ledText: String?
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
+    }
+    
+    private func configureView() {
+        if let ledText = self.ledText {
+            self.textField.text = ledText
+        }
+        self.changeTextColor(color: textColor)
+        self.changeBackgroundColor(color: backgroundColor )
     }
     
     @IBAction func tapTextColorButton(_ sender: UIButton) {
         if sender == self.yellowButton {
             self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.purpleButton {
             self.changeTextColor(color: .purple)
+            self.textColor = .purple
         } else {
             self.changeTextColor(color: .green)
+            self.textColor = .green
         }
     }
     
     @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
         if sender == self.blackButton {
             self.changeBackgroundColor(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueButton {
             self.changeBackgroundColor(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColor(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     
     @IBAction func tapSaveButton(_ sender: UIButton) {
-         
+        self.delegate?.changeSetting(
+            text: self.textField.text,
+            textColor: self.textColor,
+            backgroundColor: self.backgroundColor )
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func changeTextColor(color : UIColor){
--- Part2 Basic/chap2 LED 전광판 앱/LEDBoard/LEDBoard/ViewController.swift
+++ Part2 Basic/chap2 LED 전광판 앱/LEDBoard/LEDBoard/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
 
     @IBOutlet weak var contentsLabel: UILabel!
     
@@ -16,6 +16,22 @@ class ViewController: UIViewController {
         self.contentsLabel.textColor = .yellow 
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black 
+        }
+    }
+    
+    func changeSetting(text: String?, textColor: UIColor?, backgroundColor: UIColor?) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
 
 }
 
